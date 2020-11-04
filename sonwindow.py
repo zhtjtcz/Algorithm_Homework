9c81679b56d9f26c8dc451ef661aef055aecb49d
zht.dellg3 <823120439@qq.com>
2020-11-04 17:58:05 +0800

    V2.2

--- sonwindow.py
+++ sonwindow.py
@@ -21,35 +21,9 @@ class WatchWindows(QtWidgets.QMainWindow):
 		self.move((screen.width()-size.width())/2,(screen.height()-size.height())/2)
 
 	def Watch(self):
-		# self.mappic.clear()
 		self.mappic.clear()
 		self.map.Draw()
 		self.mappic.setPixmap(QPixmap('b.png'))
-
-		'''
-		fig=plt.figure()
-		self.map.Draw()
-		self.pm=QPixmap(fig)
-		'''
-		# self.plt=self.mappic.addPlot(fig)
-
-		# self.plt.addItem(fig)
-
-		'''
-		plt.cla()
-		fig=plt.figure()
-		self.map.Draw()
-		cavans=FigureCanvas(fig)
-		'''
-		# cavans.resize(50,50)
-		# cavans.move(100,100)
-		# self.setCentralWidget(cavans)
-		
-		# self.wid.setLayout(fig)
-
-		# self.grid.addWidget(cavans,0,1)
-
-		# self.grid.addWidget(cavans)
 
 	def AddToolbar(self):
 		self.toolbar=self.addToolBar('Tools')
@@ -74,6 +48,10 @@ class WatchWindows(QtWidgets.QMainWindow):
 		self.toolbar.addAction(SettingAct)
 		self.toolbar.addAction(ExitAct)
 
+	def empty(self):
+		print('123')
+		pass
+
 	def Addbox(self):
 		self.box1=QGroupBox("Parameter Settings")
 		a=QRadioButton("Hello")
@@ -88,10 +66,44 @@ class WatchWindows(QtWidgets.QMainWindow):
 		self.wid=QWidget(self)
 		self.setCentralWidget(self.wid)
 		self.wid.setLayout(vbox)
+	
+	def SetLayout(self):
+		self.main_widget=QWidget()
+		self.main_layout=QGridLayout()
+		self.main_widget.setLayout(self.main_layout)
+		# Main layout set
 
+		self.right_widget=QWidget()
+		self.right_layout=QGridLayout()
+		self.right_widget.setLayout(self.right_layout)
+		#Right layout set
+
+		self.left_widget=QWidget()
+		self.left_layout=QGridLayout()
+		self.left_widget.setLayout(self.left_layout)
+		#Left layout set
+
+		self.setCentralWidget(self.main_widget)
+		self.main_layout.addWidget(self.left_widget,0,0,12,5)
+		self.main_layout.addWidget(self.right_widget,0,5,12,7)
+
+		self.mappic=QLabel()
+		self.right_layout.addWidget(self.mappic,1,6,4,7)
+
+		self.nodenum=QLineEdit()
+		self.nodenum.setPlaceholderText("Nodes sum")
+		self.nodenum.returnPressed.connect(self.empty)
+		self.left_layout.addWidget(self.nodenum,1,0,1,5)
+
+		self.edgesnum=QLineEdit()
+		self.edgesnum.setPlaceholderText("Edges sum")
+		self.edgesnum.returnPressed.connect(self.empty)
+		self.left_layout.addWidget(self.edgesnum,2,0,1,5)
+
+	
 	def __init__(self):
 		super().__init__()
-		self.resize(800,800)
+		self.resize(1200,800)
 		self.setWindowTitle("Simple Map")
 		self.center()
 		self.setWindowOpacity(0.9)
@@ -101,18 +113,6 @@ class WatchWindows(QtWidgets.QMainWindow):
 		self.map.CreateNolmalMap()
 		
 		self.AddToolbar()
-		self.main_widget=QWidget()
-		self.main_layout=QGridLayout()
-		self.main_widget.setLayout(self.main_layout)
-		
-		self.right_widget=QWidget()
-		self.right_layout=QGridLayout()
-		self.right_widget.setLayout(self.right_layout)
-
-		self.setCentralWidget(self.main_widget)
-		self.main_layout.addWidget(self.right_widget, 0, 5, 12, 7)
-
-		self.mappic=QLabel()
-		self.right_layout.addWidget(self.mappic,1,6,4,7)
+		self.SetLayout()
 
 		# self.Addbox()
